8533173630a3c558dfe05bd54ca600e030a39f0b
Kyle Bendickson <kjbendickson@gmail.com>
2021-01-07 16:35:44 -0800

    Remove dead notes

--- api/src/main/java/org/apache/iceberg/util/UnicodeUtil.java
+++ api/src/main/java/org/apache/iceberg/util/UnicodeUtil.java
@@ -22,8 +22,6 @@ package org.apache.iceberg.util;
 import org.apache.iceberg.expressions.Literal;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 
-// TODO(kbendick) - Can we possibly use the unicode high-surrogates for a bound and would
-//                  that provide any value?
 public class UnicodeUtil {
   // not meant to be instantiated
   private UnicodeUtil() {
