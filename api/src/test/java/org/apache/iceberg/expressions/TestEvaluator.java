8d0700c0da7304b15340fb5144e1ecf47cb38a52
Kyle Bendickson <kjbendickson@gmail.com>
2021-01-07 16:42:12 -0800

    Remove unneeded extra import

--- api/src/test/java/org/apache/iceberg/expressions/TestEvaluator.java
+++ api/src/test/java/org/apache/iceberg/expressions/TestEvaluator.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 import org.apache.avro.util.Utf8;
 import org.apache.iceberg.AssertHelpers;
 import org.apache.iceberg.TestHelpers;
-import org.apache.iceberg.TestHelpers.Row;
 import org.apache.iceberg.exceptions.ValidationException;
 import org.apache.iceberg.types.Types;
 import org.apache.iceberg.types.Types.StructType;
@@ -227,18 +226,18 @@ public class TestEvaluator {
   public void testStartsWith() {
     StructType struct = StructType.of(required(24, "s", Types.StringType.get()));
     Evaluator evaluator = new Evaluator(struct, startsWith("s", "abc"));
-    Assert.assertTrue("abc startsWith abc => true", evaluator.eval(Row.of("abc")));
-    Assert.assertFalse("abc startsWith Abc => false", evaluator.eval(Row.of("Abc")));
-    Assert.assertFalse("a startsWith abc => false", evaluator.eval(Row.of("a")));
+    Assert.assertTrue("abc startsWith abc => true", evaluator.eval(TestHelpers.Row.of("abc")));
+    Assert.assertFalse("abc startsWith Abc => false", evaluator.eval(TestHelpers.Row.of("Abc")));
+    Assert.assertFalse("a startsWith abc => false", evaluator.eval(TestHelpers.Row.of("a")));
   }
 
   @Test
   public void testNotStartsWith() {
     StructType struct = StructType.of(required(24, "s", Types.StringType.get()));
     Evaluator evaluator = new Evaluator(struct, notStartsWith("s", "abc"));
-    Assert.assertFalse("abc notStartsWith abc => false", evaluator.eval(Row.of("abc")));
-    Assert.assertTrue("abc notStartsWith Abc => true", evaluator.eval(Row.of("Abc")));
-    Assert.assertTrue("a notStartsWith abc => true", evaluator.eval(Row.of("a")));
+    Assert.assertFalse("abc notStartsWith abc => false", evaluator.eval(TestHelpers.Row.of("abc")));
+    Assert.assertTrue("abc notStartsWith Abc => true", evaluator.eval(TestHelpers.Row.of("Abc")));
+    Assert.assertTrue("a notStartsWith abc => true", evaluator.eval(TestHelpers.Row.of("a")));
   }
 
   @Test
