b0e7a0cbb282b38599ce33eacd11df9aca59044c
Kyle Bendickson <kjbendickson@gmail.com>
2021-01-09 15:49:08 -0800

    Remove flakey string test and open up a follow up ticket in iceberg instead

--- api/src/test/java/org/apache/iceberg/expressions/TestExpressionBinding.java
+++ api/src/test/java/org/apache/iceberg/expressions/TestExpressionBinding.java
@@ -38,7 +38,6 @@ import static org.apache.iceberg.expressions.Expressions.not;
 import static org.apache.iceberg.expressions.Expressions.notStartsWith;
 import static org.apache.iceberg.expressions.Expressions.or;
 import static org.apache.iceberg.expressions.Expressions.startsWith;
-import static org.apache.iceberg.types.Types.NestedField.optional;
 import static org.apache.iceberg.types.Types.NestedField.required;
 
 public class TestExpressionBinding {
@@ -46,7 +45,7 @@ public class TestExpressionBinding {
       required(0, "x", Types.IntegerType.get()),
       required(1, "y", Types.IntegerType.get()),
       required(2, "z", Types.IntegerType.get()),
-      optional(3, "data", Types.StringType.get())
+      required(3, "data", Types.StringType.get())
   );
 
   @Test
@@ -191,21 +190,6 @@ public class TestExpressionBinding {
     Expression bound = Binder.bind(STRUCT, not(not(lessThan("y", 100))));
     BoundPredicate<?> pred = TestHelpers.assertAndUnwrap(bound);
     Assert.assertEquals("Should have the correct bound field", 1, pred.term().ref().fieldId());
-
-    // TODO(kbendick) - We need to implement proper equals etc on Expression (plus subclasses) and for Predicate, Bound.
-    //                  I will do this in a separate ticket with an @Ignore on this test for now.
-    //  org.apache.iceberg.expressions.TestExpressionBinding > testBasicSimplification FAILED
-    //    java.lang.AssertionError: Should simplify true and not(pred) to not(pred)
-    //    expected: org.apache.iceberg.expressions.Not<not(ref(id=3, accessor-type=string) startsWith ""abc"")>
-    //    but was: org.apache.iceberg.expressions.Not<not(ref(id=3, accessor-type=string) startsWith ""abc"")>
-    String original = Binder.bind(STRUCT, and(alwaysTrue(), not(startsWith("data", "abc")))).toString();
-    String simplified = Binder.bind(STRUCT, not(startsWith("data", "abc"))).toString();
-//    Assert.assertEquals("Should simplify true and not(pred) to not(pred)",
-//            Binder.bind(STRUCT, not(startsWith("data", "abc"))),
-//            Binder.bind(STRUCT, and(alwaysTrue(), not(startsWith("data", "abc")))));
-    Assert.assertEquals("Should simplify true and not(pred) to not(pred)",
-            simplified,
-            original);
   }
 
   @Test
