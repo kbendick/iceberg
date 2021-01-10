5da85a92944a53291751087cef73a071ff858568
Kyle Bendickson <kjbendickson@gmail.com>
2021-01-09 17:35:24 -0800

    Revert spacing change due to autoformatting

--- api/src/main/java/org/apache/iceberg/expressions/InclusiveMetricsEvaluator.java
+++ api/src/main/java/org/apache/iceberg/expressions/InclusiveMetricsEvaluator.java
@@ -46,8 +46,8 @@ import static org.apache.iceberg.expressions.Expressions.rewriteNot;
  * return value of {@code eval} is false.
  */
 public class InclusiveMetricsEvaluator {
-
   private static final int IN_PREDICATE_LIMIT = 200;
+
   private final Expression expr;
 
   public InclusiveMetricsEvaluator(Schema schema, Expression unbound) {
